e47d31823b0745a867ebdc437abd6836dab9456b
Will G <sniper7kills@users.noreply.github.com>
2025-10-13 17:07:52 +0000

    change default authorizaton mode

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -116,7 +116,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'identityPool',
+    defaultAuthorizationMode: 'userPool',
   },
 });
 
