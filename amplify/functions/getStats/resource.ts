26b58f8341f4a1ef48cc6597afe912f48247cdac
Will G <sniper7kills@users.noreply.github.com>
2025-10-14 04:23:19 +0000

    remove circular depds

--- amplify/functions/getStats/resource.ts
+++ amplify/functions/getStats/resource.ts
@@ -3,6 +3,7 @@ import { defineFunction } from '@aws-amplify/backend';
 export const getStats = defineFunction({
   name: 'getStats',
   entry: './handler.ts',
+  resourceGroupName: 'data',
   environment: {
     // This will be populated by backend.ts
   },
